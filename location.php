f1b26a2035250606385167c774d603ec50e084c4
OliverStuP <62386909+OliverStuP@users.noreply.github.com>
2020-06-20 16:18:05 +0200

    Update location.php

--- location.php
+++ location.php
@@ -1,6 +1,6 @@
 <?php
 define('ISITSAFETORUN', TRUE);
-define("TITLE", "Location | Haute Vue");
+define("TITLE", "Location | CS Pets");
 include('includes/header.php');
 $valid   = TRUE; 
 ?>
@@ -13,11 +13,12 @@ $valid   = TRUE;
       
       <section>
       <h1>Neighbourhood:</h1>
-       <p>We live down a quiet country road where tractors are almost the only form of traffic! Our own fields are all that surrounds us; one is a walnut plantation which you are free to wander around. The rest is grass and fruit trees so we have no neighbours for you to worry about!</p>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo nisi eu massa lacinia, eget eleifend turpis viverra. Aenean nec sagittis sem, vitae bibendum risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt urna augue, vitae egestas metus semper sed. Aenean varius tincidunt pellentesque. Nam molestie dignissim nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies finibus dolor ut ultricies. Donec venenatis libero non lacus consequat vestibulum. 
+</p>
       <picture>
          <img id="map_image" alt="Location of Montgaillard" src="https://static-maps.yandex.ru/1.x/?l=map&lang=en_US&ll=0.874672,43.929810&z=10&size=600,300&pt=0.874672,43.929810,pm2rdl1">
           <img id="map_image_small" alt="Location of Montgaillard" src="https://static-maps.yandex.ru/1.x/?l=map&lang=en_US&ll=0.874672,43.929810&z=10&size=275,275&pt=0.874672,43.929810,pm2rdl1">
-       <figcaption>Exact location information is provided after a booking is confirmed.</figcaption>
+       <figcaption>Our location via GPS</figcaption>
       </picture>
       </section>
     </main>
