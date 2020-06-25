20a2d61c5c0f124fdc5b83b1f76ee4a35af4452e
OliverStuP <62386909+OliverStuP@users.noreply.github.com>
2020-06-25 12:39:53 +0200

    Update location.php

--- location.php
+++ location.php
@@ -13,7 +13,9 @@ $valid   = TRUE;
       
       <section>
       <h1>Neighbourhood:</h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo nisi eu massa lacinia, eget eleifend turpis viverra. Aenean nec sagittis sem, vitae bibendum risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt urna augue, vitae egestas metus semper sed. Aenean varius tincidunt pellentesque. Nam molestie dignissim nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies finibus dolor ut ultricies. Donec venenatis libero non lacus consequat vestibulum. 
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo nisi eu massa lacinia, eget eleifend turpis viverra. Aenean nec sagittis sem, vitae bibendum risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+    
+<p>Aliquam tincidunt urna augue, vitae egestas metus semper sed. Aenean varius tincidunt pellentesque. Nam molestie dignissim nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies finibus dolor ut ultricies. Donec venenatis libero non lacus consequat vestibulum. 
 </p>
       <picture>
          <img id="map_image" alt="Location of Montgaillard" src="https://static-maps.yandex.ru/1.x/?l=map&lang=en_US&ll=0.874672,43.929810&z=10&size=600,300&pt=0.874672,43.929810,pm2rdl1">
